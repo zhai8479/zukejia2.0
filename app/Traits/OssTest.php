48059f4f4345b7788ae062224c73e3ca0b08e867
zhan <390961827@qq.com>
2018-04-23 20:38:08 +0800

    修改缓存

--- app/Traits/OssTest.php
+++ app/Traits/OssTest.php
@@ -5,3 +5,33 @@
  * Date: 2018/4/23
  * Time: 15:07
  */
+
+namespace App\Traits;
+
+use JohnLui\AliyunOSS;
+
+class OSSTest
+{
+    private $ossClient;
+    private static $bucketName;
+
+    public function __construct($isInternal = false)
+    {
+        $serverAddress = $isInternal ? config('alioss.ossServerInternal') : config('alioss.ossServer');
+        $this->ossClient = AliyunOSS::boot(
+            $serverAddress,
+            config('alioss.AccessKeyId'),
+            config('alioss.AccessKeySecret')
+        );
+    }
+
+    public static function upload($ossKey, $filePath,$BucketName)
+    {
+        $oss = new OSSTest(false); // 上传文件使用内网，免流量费
+        $oss->ossClient->setBucket($BucketName);
+        $res = $oss->ossClient->uploadFile($ossKey, $filePath);
+        return $res;
+    }
+
+
+}
