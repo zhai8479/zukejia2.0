8dd601478e0f7137493b7af1f4933822288aab95
zhan <390961827@qq.com>
2018-04-24 20:28:55 +0800

    加入服务

---
+++ app/Providers/OssServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Providers;
+
+use App\Library\OSS;
+use Illuminate\Support\ServiceProvider;
+
+class OssServiceProvider extends ServiceProvider
+{
+    protected $defer = true;
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(Password::class, function () {
+            return new OSS();
+        });
+
+        $this->app->bind('App\Library\OSS', function () {
+            return new OSS();
+        });
+    }
+
+    public function provides()
+    {
+        return [OSS::class];
+    }
+}
