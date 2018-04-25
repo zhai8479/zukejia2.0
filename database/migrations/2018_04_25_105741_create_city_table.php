0275e25b1e7330f7c5184a316fbe8dd26329865c
Amos <847906819@qq.com>
2018-04-25 13:43:58 +0800

    添加城市列表

---
+++ database/migrations/2018_04_25_105741_create_city_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCityTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('city', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->smallInteger('parent_id')->length(5);
+            $table->tinyInteger('is_show')->comment('是否显示  0显示1不显示')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('city');
+    }
+}
