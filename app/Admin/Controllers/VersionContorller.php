0319d58e56605b761bbdb6b563b3dd71bfbd621d
zhan <390961827@qq.com>
2018-05-03 11:17:54 +0800

    app upload file

--- app/Admin/Controllers/VersionContorller.php
+++ app/Admin/Controllers/VersionContorller.php
@@ -36,7 +36,8 @@ class VersionContorller extends BaseController
             $content->header('发布新版本');
             $content->description('发布');
             $oss = new OSSHelp(false);
-            $oss->uploadFile('zkj-static','sdk/zukehouse-app-release.apk','');
+            $path = getcwd() . '/file/';
+            $oss->uploadFile('zkj-static','sdk/zukehouse-app-release.apk',$path.'zukehouse-app-release.apk');
             $content->body($this->form());
         });
     }
